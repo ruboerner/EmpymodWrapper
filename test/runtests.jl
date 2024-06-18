5414f494d767f5050c5f793247d4b65d1e266fda
Ralph-Uwe Börner <rub@geophysik.tu-freiberg.de>
2024-06-18 14:28:09 +0200

    WIP
    - Add test for loop source, TDEM

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,11 @@
 using EmpymodWrapper
 using Test
+
+@testset "empymod loop source time domain" begin
+    offset = 100.0
+    hz = empymod_loop(
+        src=[0, 0, 0, 0, 90],
+        rec=[offset, 0, 0, 0, 90],
+        depth=0, res=[2e14, 100], freqtime=1e-3, verb=0)
+    @test hz ≈ -1 / (4 * pi * offset^3)
+end
